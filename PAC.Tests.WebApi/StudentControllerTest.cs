5e742309faff4fe12909e824376d3d2edd8b22d2
Nicolás Sogliano <51862567+nsogliano@users.noreply.github.com>
2023-10-12 19:51:52 -0300

    pkg controllers

--- PAC.Tests.WebApi/StudentControllerTest.cs
+++ PAC.Tests.WebApi/StudentControllerTest.cs
@@ -11,8 +11,39 @@ using Microsoft.AspNetCore.Mvc;
 [TestClass]
 public class StudentControllerTest
 {
+        private Mock<IStudentLogic> studentLogicMock;
+        private StudentController controller;
+
         [TestInitialize]
-        public void InitTest()
+        public void Initialize()
         {
+                studentLogicMock = new Mock<IStudentLogic>();
+                controller = new StudentController(studentLogicMock.Object);
+        }
+
+        [TestMethod]
+        public void PostStudentOk()
+        {
+                var student = new Student();
+                studentLogicMock.Setup(x => x.InsertStudents(student));
+
+                var result = controller.PostStudent(student);
+
+                var objectResult = result as ObjectResult;
+                Assert.IsNotNull(objectResult);
+                Assert.AreEqual(200, objectResult.StatusCode);
+        }
+
+        [TestMethod]
+        public void PostStudentFail()
+        {
+                var student = new Student();
+                studentLogicMock.Setup(x => x.InsertStudents(student)).Throws(new Exception());
+
+                var result = controller.PostStudent(student);
+
+                var objectResult = result as ObjectResult;
+                Assert.IsNotNull(objectResult);
+                Assert.AreEqual(400, objectResult.StatusCode);
         }
 }
